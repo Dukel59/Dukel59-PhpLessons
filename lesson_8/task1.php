9cce88baa6eca93c02b7a501cd7b324b83eb24cf
Aliaksandr Zakharevich <jonnydukalez@gmail.com>
2023-02-01 18:15:11 +0300

    added filter by country

--- lesson_8/task1.php
+++ lesson_8/task1.php
@@ -7,24 +7,38 @@ foreach ($peoples as $line) {
     $data[] = str_getcsv($line);
 }
 
-if ($_POST['name']) {
+$name = $_POST['name'];
+$fullName = $_POST['fullName'];
+$country = $_POST['country'];
+
+$searchPeoples = $data;
+
+if ($name) {
+    $temp = $searchPeoples;
     $searchPeoples = [];
-    if($_POST['fullName']){
-        foreach ($data as $people) {
-            if ($people[0] == $_POST['name']) {
+    if ($fullName) {
+        foreach ($temp as $people) {
+            if ($people[0] == $name) {
                 $searchPeoples[] = $people;
             }
         }
     } else {
-        foreach ($data as $people) {
-            if (str_contains($people[0], $_POST['name'])) {
+        foreach ($temp as $people) {
+            if (str_contains($people[0], $name)) {
                 $searchPeoples[] = $people;
             }
         }
     }
+}
 
-} else {
-    $searchPeoples = $data;
+if ($country) {
+    $temp = $searchPeoples;
+    $searchPeoples = [];
+    foreach ($temp as $people) {
+        if ($people[4] == $country) {
+            $searchPeoples[] = $people;
+        }
+    }
 }
 
 ?>
@@ -34,21 +48,32 @@ if ($_POST['name']) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Codes</title>
 </head>
 <body>
 <div class="container">
     <form action="" method="POST">
         <div class="form-group">
+            <label>Enter course</label>
+            <select class="form-control" name="country">
+                <?php foreach ($data as $people) : ?>
+                    <?php if ($people[4] == $country) : ?>
+                        <option selected value="<?= $people[4] ?>"><?= $people[4] ?> </option>
+                    <?php else: ?>
+                        <option value="<?= $people[4] ?>"><?= $people[4] ?> </option>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </select>
             <label>Enter name</label>
             <input type="text" name="name" class="form-control" placeholder="Enter name"
-                <?php if ($_POST['name']) : ?>
-                    echo value="<?php echo $_POST['name']?>"
+                <?php if ($name) : ?>
+                    echo value="<?php echo $name ?>"
                 <?php endif; ?>
             >
             <input type="checkbox" name="fullName" id="checkbox"
-                <?php if ($_POST['fullName']) : ?>
+                <?php if ($fullName) : ?>
                     echo checked
                 <?php endif; ?>
             >
